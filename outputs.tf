1dfba905a8f52988a675066bfd2e29e21f1a4abd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:28 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "app_service_virtual_network_swift_connections_id" {
+  description = "Map of id values across all app_service_virtual_network_swift_connections, keyed the same as var.app_service_virtual_network_swift_connections"
+  value       = { for k, v in azurerm_app_service_virtual_network_swift_connection.app_service_virtual_network_swift_connections : k => v.id }
+}
 output "app_service_virtual_network_swift_connections_app_service_id" {
   description = "Map of app_service_id values across all app_service_virtual_network_swift_connections, keyed the same as var.app_service_virtual_network_swift_connections"
   value       = { for k, v in azurerm_app_service_virtual_network_swift_connection.app_service_virtual_network_swift_connections : k => v.app_service_id }
