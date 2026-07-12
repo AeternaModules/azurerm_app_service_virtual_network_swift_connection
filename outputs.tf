45a5796120f9492799d87360a7b17a8e8abf5c6e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:46 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "app_service_virtual_network_swift_connections" {
-  description = "All app_service_virtual_network_swift_connection resources"
-  value       = azurerm_app_service_virtual_network_swift_connection.app_service_virtual_network_swift_connections
-}
 output "app_service_virtual_network_swift_connections_app_service_id" {
-  description = "List of app_service_id values across all app_service_virtual_network_swift_connections"
-  value       = [for k, v in azurerm_app_service_virtual_network_swift_connection.app_service_virtual_network_swift_connections : v.app_service_id]
+  description = "Map of app_service_id values across all app_service_virtual_network_swift_connections, keyed the same as var.app_service_virtual_network_swift_connections"
+  value       = { for k, v in azurerm_app_service_virtual_network_swift_connection.app_service_virtual_network_swift_connections : k => v.app_service_id }
 }
 output "app_service_virtual_network_swift_connections_subnet_id" {
-  description = "List of subnet_id values across all app_service_virtual_network_swift_connections"
-  value       = [for k, v in azurerm_app_service_virtual_network_swift_connection.app_service_virtual_network_swift_connections : v.subnet_id]
+  description = "Map of subnet_id values across all app_service_virtual_network_swift_connections, keyed the same as var.app_service_virtual_network_swift_connections"
+  value       = { for k, v in azurerm_app_service_virtual_network_swift_connection.app_service_virtual_network_swift_connections : k => v.subnet_id }
 }
 
